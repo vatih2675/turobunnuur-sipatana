d335716fd1c480b142c2a0903f69d38f880f3bec
vatih2675 <ruslansamuel11@gmail.com>
2026-02-06 11:09:09 +0800

    Edit Daftar Pelaksana Puasa

--- src/components/grid/BukaPuasa.jsx
+++ src/components/grid/BukaPuasa.jsx
@@ -1,6 +1,8 @@
 import { formatTanggal } from "../../constants/functions"; 
 
 export default function BukaPuasa({bp}) {
+  let waktu = new Date();
+  let tahun = waktu.getFullYear();
   return (
     <div className="flex flex-col px-1 py-1 hover:bg-green-200 hover:shadow transition-color duration-300 rounded-xl">
       <span className="text-[0.6rem] text-gray-600 bg-green-100 rounded-l-full rounded-r-full px-1 w-fit">
@@ -10,7 +12,7 @@ export default function BukaPuasa({bp}) {
         <i className="bi-check2-circle"></i>
         <div className="flex flex-col justify-start items-start">
           <p>Pelaksana buka puasa hari ini</p>
-          <p>{bp.hijriyah < 10 ? '0' + bp.hijriyah : bp.hijriyah} Ramadhan 1445 H</p>
+          <p>{bp.hijriyah < 10 ? '0' + bp.hijriyah : bp.hijriyah} Ramadhan {tahun - 579} H</p>
           <p className="font-black">{bp.nama}</p>
         </div>
       </div>
--- src/constants/ramadhan.jsx
+++ src/constants/ramadhan.jsx
@@ -1,152 +1,152 @@
 export const pelaksanaBukaPuasa = [
-    {
-        hijriyah: 1,
-        masehi: "2/18/26",
-        nama: "SOFYAN BUTOLO, RONI PODUNGGE, MOH. TAUFIK, IFANA B., WASKITO B., FITRIA PODUNGGE"
-    },
-    {
-        hijriyah: 2,
-        masehi: "2/19/26",
-        nama: "IBU LINDA, Hj. TALHA HASIRU, RISNA MBUTI, AKON ISHAK, HAMDAN ISHAK"
-    },
-    {
-        hijriyah: 3,
-        masehi: "2/20/26",
-        nama: "ZAINAL SALEH, DODI ISHAK, YUSUF ISHAK, NANANG ISHAK, IBU MARTA"
-    },
-    {
-        hijriyah: 4,
-        masehi: "2/21/26",
-        nama: "UUN KOLLY, JAYADI HUSAIN, SABRINA HUSAIN, Hj. INA NOHOLO, ASNAWI PODUNGGE"
-    },
-    {
-        hijriyah: 5,
-        masehi: "2/22/26",
-        nama: "H. ABDULLAH N. TUE, ISMET HELINGO, ALFIAN HELINGO"
-    },
-    {
-        hijriyah: 6,
-        masehi: "2/23/26",
-        nama: "KEL. (Alm.) Hj. NUN T., Hj. RAMLA TOBAMBA, NAZIB T., MOH. ZUHRI T., BAS UTON"
-    },
-    {
-        hijriyah: 7,
-        masehi: "2/24/26",
-        nama: "KEL. LANTU, RIDWAN IDRAK, YUSRI IDRAK, LINDA MIOLO, FAUZIAH TUU"
-    },
-    {
-        hijriyah: 8,
-        masehi: "2/25/26",
-        nama: "KEL. NOKI, MARGARETA KATILI, H. MUKROM BAKRI, RAMLA HOU"
-    },
-    {
-        hijriyah: 9,
-        masehi: "2/26/26",
-        nama: "Hj. NIKMA MIOLO, H. ISMAIL MIOLO, FARID MANOPO, HARRY GOBEL"
-    },
-    {
-        hijriyah: 10,
-        masehi: "2/27/26",
-        nama: "ARIFIN HELINGO, KEL. PODUNGGE - POLONTALO, KEL. (Alm.) MASKUR HELINGO"
-    },
-    {
-        hijriyah: 11,
-        masehi: "2/28/26",
-        nama: "KEL. JAHJA"
-    },
-    {
-        hijriyah: 12,
-        masehi: "3/1/26",
-        nama: "SONI LIHAWA, KA ARI, Hj. HIJIRI IBRAHIM, MIS GANI, KEL. KIDO"
-    },
-    {
-        hijriyah: 13,
-        masehi: "3/2/26",
-        nama: "OM YAN, ELVI ISMAIL, ZABIR LUAWO, RITA ISMAIL, UDIN HAMUL"
-    },
-    {
-        hijriyah: 14,
-        masehi: "3/3/26",
-        nama: "HELMI PODUNGGE, OM RUDI, WINDRA PODUNGGE, ABDUL RAHMAN PODUNGGE"
-    },
-    {
-        hijriyah: 15,
-        masehi: "3/4/26",
-        nama: "KEL. HARUN, KEL. PANEO, KEL. HELINGO, RUSTAM KASIM"
-    },
-    {
-        hijriyah: 16,
-        masehi: "3/5/26",
-        nama: "Hj. FADLINA PODUNGGE, MADI INA, KEL. TAHIR MOHAMAD, (Alm.) H. SULEMAN KALLY"
-    },
-    {
-        hijriyah: 17,
-        masehi: "3/6/26",
-        nama: "KEL. (Alm.) BAKARI HELINGO – TOLINGGI, YAYUK VAN SOLANG"
-    },
-    {
-        hijriyah: 18,
-        masehi: "3/7/26",
-        nama: "IMAN B. ANTU, NINGSIH PODUNGGE, KEL. MATULU, CENDA GAFUR"
-    },
-    {
-        hijriyah: 19,
-        masehi: "3/8/26",
-        nama: "KEL. (Alm.) SUPRIO BUTOLO – KEL. HELINGO"
-    },
-    {
-        hijriyah: 20,
-        masehi: "3/9/26",
-        nama: "KEL. HASAN – ABDULLAH, ANDREAS LIARIAN"
-    },
-    {
-        hijriyah: 21,
-        masehi: "3/10/26",
-        nama: "ZULKIFLI AKASE, KEL. MAKU, MAS TOTOK, H. MUKROM BAKRI"
-    },
-    {
-        hijriyah: 22,
-        masehi: "3/11/26",
-        nama: "KEL. (Alm.) H. EKA PODUNGGE"
-    },
-    {
-        hijriyah: 23,
-        masehi: "3/12/26",
-        nama: "KEL. TONIYANTO KASIM, INTAN S. KASIM, CRYSTAL KASIM, MUTIARA M. KASIM "
-    },
-    {
-        hijriyah: 24,
-        masehi: "3/13/26",
-        nama: "Hj. SARIPI KASIM, BAGAS SETIAWAN, DESI KASIM, ANDA LAYA, HALIM LAYA"
-    },
-    {
-        hijriyah: 25,
-        masehi: "3/14/26",
-        nama: "H. FAHMI TALIB, RAJIMAN PANEO, RIDA TOBAMBA, H. AZHAR DODA, Hj. NENO H., POPPY MILE"
-    },
-    {
-        hijriyah: 26,
-        masehi: "3/15/26",
-        nama: "SABRIN GANI, HARUN GANI, JEMI GANI, RUSLAN GANI, NUZLAN GANI"
-    },
-    {
-        hijriyah: 27,
-        masehi: "3/16/26",
-        nama: "Hj. SURAIYA TAHIR, Hj. RUMI TAHIR MBA YANJ, NITA KOLLY, H. MUKROM BAKRI"
-    },
-    {
-        hijriyah: 28,
-        masehi: "3/17/26",
-        nama: "KEL. NURDIN BAINO, KEL. TUE, RASID GIONTE, KEL. MBUTI – KEL. LIHAWA"
-    },
-    {
-        hijriyah: 29,
-        masehi: "3/18/26",
-        nama: "MUSADIQ BASALAMA, FAHRI BASALAMA, LUTFI UMAR"
-    },
-    {
-        hijriyah: 30,
-        masehi: "3/19/26",
-        nama: "KEL. (Alm.) WENNY JAHJA, Hj. LINDA JAHJA, SALIM GANI"
-    }
-]
+  {
+    hijriyah: 1,
+    masehi: "2/18/26",
+    nama: "SOFYAN BUTOLO, RONI PODUNGGE, MOH. TAUFIK, IFANA B., WASKITO B., FITRIA PODUNGGE",
+  },
+  {
+    hijriyah: 2,
+    masehi: "2/19/26",
+    nama: "IBU LINDA, Hj. TALHA HASIRU, RISNA MBUTI, AKON ISHAK, HAMDAN ISHAK",
+  },
+  {
+    hijriyah: 3,
+    masehi: "2/20/26",
+    nama: "ZAINAL SALEH, DODI ISHAK, YUSUF ISHAK, NANANG ISHAK, IBU MARTA",
+  },
+  {
+    hijriyah: 4,
+    masehi: "2/21/26",
+    nama: "HANAKO KOLLY, UUN KOLLY, JAYADI HUSAIN, SABRINA HUSAIN, Hj. INA NOHOLO, ASNAWI PODUNGGE",
+  },
+  {
+    hijriyah: 5,
+    masehi: "2/22/26",
+    nama: "H. ABDULLAH N. TUE, ISMET HELINGO, ALFIAN HELINGO, Hj. LINDA YAHYA",
+  },
+  {
+    hijriyah: 6,
+    masehi: "2/23/26",
+    nama: "KEL. (Alm.) Hj. NUN T., Hj. RAMLA TOBAMBA, NAZIB T., MOH. ZUHRI T., BAS UTON",
+  },
+  {
+    hijriyah: 7,
+    masehi: "2/24/26",
+    nama: "KEL. LANTU, RIDWAN IDRAK, YUSRI IDRAK, LINDA MIOLO, FAUZIAH TUU, Ibu DESI SUGIWANTI",
+  },
+  {
+    hijriyah: 8,
+    masehi: "2/25/26",
+    nama: "KEL. NOKI, MARGARETA KATILI, RAMLA HOU",
+  },
+  {
+    hijriyah: 9,
+    masehi: "2/26/26",
+    nama: "Hj. NIKMA MIOLO, H. ISMAIL MIOLO, FARID MANOPO, HARRY GOBEL",
+  },
+  {
+    hijriyah: 10,
+    masehi: "2/27/26",
+    nama: "KEL. ARIFIN HELINGO, Hj. LIKO PODUNGGE, KEL. (Alm.) MASKUR HELINGO",
+  },
+  {
+    hijriyah: 11,
+    masehi: "2/28/26",
+    nama: "KEL. JAHJA",
+  },
+  {
+    hijriyah: 12,
+    masehi: "3/1/26",
+    nama: "SONI LIHAWA, KA ARI, Hj. HIJIRI IBRAHIM, MIS GANI, KEL. KIDO",
+  },
+  {
+    hijriyah: 13,
+    masehi: "3/2/26",
+    nama: "OM YAN, ELVI ISMAIL, ZABIR LUAWO, RITA ISMAIL, UDIN HAMUL",
+  },
+  {
+    hijriyah: 14,
+    masehi: "3/3/26",
+    nama: "HELMI PODUNGGE, OM RUDI, WINDRA PODUNGGE, ABDUL RAHMAN PODUNGGE",
+  },
+  {
+    hijriyah: 15,
+    masehi: "3/4/26",
+    nama: "KEL. HARUN, KEL. PANEO, RUSTAM KASIM",
+  },
+  {
+    hijriyah: 16,
+    masehi: "3/5/26",
+    nama: "Hj. FADLINA PODUNGGE, MADI INA, KEL. TAHIR MOHAMAD, KEL. (Alm.) H. SULEMAN KALLY",
+  },
+  {
+    hijriyah: 17,
+    masehi: "3/6/26",
+    nama: "KEL. (Alm.) BAKARI HELINGO – TOLINGGI, YAYUK VAN SOLANG",
+  },
+  {
+    hijriyah: 18,
+    masehi: "3/7/26",
+    nama: "IMAN B. ANTU, NINGSIH PODUNGGE, KEL. MATULU, CENDA GAFUR",
+  },
+  {
+    hijriyah: 19,
+    masehi: "3/8/26",
+    nama: "KEL. (Alm.) SUPRIO BUTOLO – KEL. HELINGO",
+  },
+  {
+    hijriyah: 20,
+    masehi: "3/9/26",
+    nama: "KEL. HASAN – ABDULLAH, ANDREAS LIARIAN",
+  },
+  {
+    hijriyah: 21,
+    masehi: "3/10/26",
+    nama: "ZULKIFLI AKASE, KEL. MAKU, MAS TOTOK",
+  },
+  {
+    hijriyah: 22,
+    masehi: "3/11/26",
+    nama: "KEL. (Alm.) H. EKA PODUNGGE",
+  },
+  {
+    hijriyah: 23,
+    masehi: "3/12/26",
+    nama: "KEL. TONIYANTO KASIM, INTAN S. KASIM, CRYSTAL KASIM, MUTIARA M. KASIM ",
+  },
+  {
+    hijriyah: 24,
+    masehi: "3/13/26",
+    nama: "Hj. SARIPI KASIM, BAGAS SETIAWAN, DESI KASIM, ANDA LAYA, HALIM LAYA, UNA LAYA",
+  },
+  {
+    hijriyah: 25,
+    masehi: "3/14/26",
+    nama: "H. FAHMI TALIB, RAJIMAN PANEO, RIDA TOBAMBA, H. AZHAR DODA, Hj. NENO H., POPPY MILE",
+  },
+  {
+    hijriyah: 26,
+    masehi: "3/15/26",
+    nama: "SABRIN GANI, HARUN GANI, JEMI GANI, RUSLAN GANI, NUZLAN GANI, SALIM GANI",
+  },
+  {
+    hijriyah: 27,
+    masehi: "3/16/26",
+    nama: "Hj. SURAIYA TAHIR, Hj. RUMI TAHIR, Mba YANI, NITA KOLLY",
+  },
+  {
+    hijriyah: 28,
+    masehi: "3/17/26",
+    nama: "KEL. NURDIN BAINO, KEL. TUE, RASID GIONTE, KEL. MBUTI – KEL. LIHAWA",
+  },
+  {
+    hijriyah: 29,
+    masehi: "3/18/26",
+    nama: "MUSADIQ BASALAMA, FAHRI BASALAMA, LUTFI UMAR",
+  },
+  {
+    hijriyah: 30,
+    masehi: "3/19/26",
+    nama: "KELUARGA BESAR CUCU / CICIT LT HELINGO",
+  },
+];
