2d16c2885356ab1b6d59c25c6bff0f6b644f82ea
vatih2675 <ruslansamuel11@gmail.com>
2026-02-07 22:27:28 +0800

    Edit Button Components

--- src/components/button/Secondary.jsx
+++ src/components/button/Secondary.jsx
@@ -1,7 +1,7 @@
 export default function ButtonSecondary({ title, icon, click }) {
   return (
     <button
-      className="px-2 sm:px-8 py-1 sm:py-2 bg-white/70 border border-green-700 rounded-l-full rounded-r-full text-green-700 hover:bg-green-100 hover:shadow-black/20 hover:shadow-md cursor-pointer text-[0.8rem] sm:text-base transition-all duration-300"
+      className="px-2 sm:px-4 py-1 sm:py-2 bg-white/70 border border-green-700 rounded-l-full rounded-r-full text-green-700 hover:bg-green-100 hover:shadow-black/20 hover:shadow-md cursor-pointer text-[0.8rem] sm:text-base transition-all duration-300"
       onClick={click}
     >
       <i className={`${icon} me-2`}></i>
